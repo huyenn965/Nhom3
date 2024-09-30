b31bfbf2621b88e5636c50e3b150de1ecc4f1e77
ThuBoo <tn6642455@gmail.com>
2024-09-30 12:55:24 +0700

    Add files via upload

---
+++ THLTHDTtuan4_cau2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.thlthdttuan4_cau2;
+
+/**
+ *
+ * @author admin
+ */
+public class THLTHDTtuan4_cau2 {
+
+    public static void main(String[] args) {
+        int i;
+        int j;
+        for(i=1;i<=10;i++){
+            System.out.println("bang cuu chuong "+ i);
+        for(j=1;j<=10;j++){
+            System.out.println(i+"x"+j+"="+i*j);
+        }
+      }
+      
+    }
+}
